032820c8f65134caaf81b6a16b650ae9cb5e8fdf
Punith K <punith.k@cerner.com>
2015-10-17 12:58:52 +0530

    First Commit

---
+++ eGit-Sample-Project-/src/com/example/project/egit/HelloEgit.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.example.project.egit;
+
+/**
+ * Sample Project to test eGit in Eclipse
+ * 
+ * @author PK043505
+ * 
+ */
+public class HelloEgit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello eGit:");
+	}
+
+}
